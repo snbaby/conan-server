9c1f8b967b04304f8e9868b87fb7eb5b9b2fea5a
wangjian <sn_baby@qq.com>
2018-04-20 00:53:31 +0800

    新建

---
+++ src/main/java/com/conan/crawler/server/post/ConanServerApplication.java
@@ -0,0 +1,12 @@
+package com.conan.crawler.server.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConanServerApplication.class, args);
+	}
+}
