7769cd44c1a06870c6b7d4e2a91472baa78f607e
wangjian <sn_baby@qq.com>
2018-04-23 05:00:23 +0800

    修改

--- src/main/java/com/conan/console/server/ConanServerApplication.java
+++ src/main/java/com/conan/console/server/ConanServerApplication.java
@@ -1,9 +1,11 @@
 package com.conan.console.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.conan.console.server.mapper")
 public class ConanServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/conan/console/server/rest/UserController.java
+++ src/main/java/com/conan/console/server/rest/UserController.java
@@ -2,32 +2,62 @@ package com.conan.console.server.rest;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.conan.console.server.entity.ResponseResult;
-import com.conan.console.server.entity.UserLoginParameters;
+import com.conan.console.server.exception.ConanException;
+import com.conan.console.server.parameter.UserLoginParameters;
+import com.conan.console.server.parameter.UserRegisterParameters;
+import com.conan.console.server.response.ResponseSuccessResult;
+import com.conan.console.server.service.UserService;
+import com.conan.console.server.utils.ConanExceptionConstants;
 
 @RestController
 @RequestMapping(value = "api/v2.0")
 public class UserController {
 
+	@Autowired
+	UserService userService;
+
 	@PostMapping("user_login")
-	public ResponseEntity<ResponseResult> userLogin(@Valid UserLoginParameters userLoginParameters, BindingResult bindingResult) {
+	@ResponseBody
+	public ResponseEntity<ResponseSuccessResult> userLogin(@Valid UserLoginParameters userLoginParameters,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
-			ResponseResult responseResult = new ResponseResult();
-			responseResult.setCode(HttpStatus.BAD_REQUEST.value());
-			responseResult.setContent(bindingResult.getFieldError());
-            return new ResponseEntity<>(responseResult,HttpStatus.BAD_REQUEST);
-        }
-		ResponseResult responseResult = new ResponseResult();
-		responseResult.setCode(HttpStatus.OK.value());
+			throw new ConanException(ConanExceptionConstants.PARAMETER_EXCEPTION_CODE,
+					ConanExceptionConstants.PARAMETER_EXCEPTION_MESSAGE, bindingResult.getFieldError(),
+					ConanExceptionConstants.PARAMETER_EXCEPTION_HTTP_STATUS);
+		}
 		
-		responseResult.setDescription("登陆成功");
-		return new ResponseEntity<>(responseResult,HttpStatus.OK);
+		
+		ResponseSuccessResult responseResult = new ResponseSuccessResult();
+		responseResult.setCode(HttpStatus.OK.value());
+		responseResult.setContent(userService.getUserInfo(userLoginParameters.getUser_phone(), userLoginParameters.getUser_passwd()));
+		responseResult.setDescription("success");
+		return new ResponseEntity<>(responseResult, HttpStatus.OK);
+	}
+
+	@PostMapping("user_register")
+	@ResponseBody
+	public ResponseEntity<ResponseSuccessResult> userRegister(@Valid UserRegisterParameters userRegisterParameters,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			throw new ConanException(ConanExceptionConstants.PARAMETER_EXCEPTION_CODE,
+					ConanExceptionConstants.PARAMETER_EXCEPTION_MESSAGE, bindingResult.getFieldError(),
+					ConanExceptionConstants.PARAMETER_EXCEPTION_HTTP_STATUS);
+		}
+		//用户注册
+		userService.registerUser(userRegisterParameters.getUser_phone(), userRegisterParameters.getUser_passwd());
+
+		ResponseSuccessResult responseResult = new ResponseSuccessResult();
+		responseResult.setCode(HttpStatus.CREATED.value());
+		responseResult.setDescription("success");
+		return new ResponseEntity<>(responseResult, HttpStatus.CREATED);
 	}
 }
