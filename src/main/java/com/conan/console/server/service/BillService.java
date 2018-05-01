221d3bf6e6f26059be68186c6b4ca567b1f6a518
wangjian <sn_baby@qq.com>
2018-05-01 08:44:17 +0800

    修改

--- src/main/java/com/conan/console/server/service/BillService.java
+++ src/main/java/com/conan/console/server/service/BillService.java
@@ -46,11 +46,11 @@ public class BillService {
 		pageInfo.setPageNo(userGetBillParameters.getCurrent_page());
 		pageInfo.setTotal(total);
 		pageInfo.setPageSize(ConanApplicationConstants.INIT_PAGE_SIZE);
-		
-		resultJsonObject.put("page_info",pageInfo);
-		
+
+		resultJsonObject.put("page_info", pageInfo);
+
 		JSONArray jsonArray = new JSONArray();
-		for(UserBill userBill:userBillList) {
+		for (UserBill userBill : userBillList) {
 			JSONObject userBillJsonObject = new JSONObject();
 			userBillJsonObject.put("bill_id", userBill.getId());
 			userBillJsonObject.put("created_at", userBill.getCreated_at());
@@ -58,33 +58,35 @@ public class BillService {
 			userBillJsonObject.put("bill_remain", userBill.getRemain_gold());
 			userBillJsonObject.put("bill_digest", userBill.getBill_digest());
 			userBillJsonObject.put("bill_status", userBill.getBill_digest());
-			if(StringUtils.isNotBlank(userBill.getBill_type())&&userBill.getBill_type().equals("1")) {//充值
-				RechargeBill rechargeBill = rechargeBillMapper.selectByPrimaryKey(userBill.getId());//BILL账单 recharge账单 cost账单使用的ID均为同一个ID
+			if (StringUtils.isNotBlank(userBill.getBill_type()) && userBill.getBill_type().equals("1")) {// 充值
+				RechargeBill rechargeBill = rechargeBillMapper.selectByPrimaryKey(userBill.getId());// BILL账单 recharge账单
+																									// cost账单使用的ID均为同一个ID
 				if (rechargeBill == null) {
 					throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
 							ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
 							ConanExceptionConstants.INTERNAL_SERVER_ERROR_HTTP_STATUS);
 				}
 				userBillJsonObject.put("bill_amount", rechargeBill.getGold_amount());
-			}else if(StringUtils.isNotBlank(userBill.getBill_type())&&userBill.getBill_type().equals("2")) {//支出
-				CostRecord costRecord = costRecordMapper.selectByPrimaryKey(userBill.getId());//BILL账单 recharge账单 cost账单使用的ID均为同一个ID
+			} else if (StringUtils.isNotBlank(userBill.getBill_type()) && userBill.getBill_type().equals("2")) {// 支出
+				CostRecord costRecord = costRecordMapper.selectByPrimaryKey(userBill.getId());// BILL账单 recharge账单
+																								// cost账单使用的ID均为同一个ID
 				if (costRecord == null) {
 					throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
 							ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
 							ConanExceptionConstants.INTERNAL_SERVER_ERROR_HTTP_STATUS);
 				}
 				userBillJsonObject.put("bill_amount", costRecord.getCost_gold());
-			}else {//异常
+			} else {// 异常
 				throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
 						ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
 						ConanExceptionConstants.INTERNAL_SERVER_ERROR_HTTP_STATUS);
 			}
 			jsonArray.add(userBillJsonObject);
 		}
-		resultJsonObject.put("bills",jsonArray);
+		resultJsonObject.put("bills", jsonArray);
 		return resultJsonObject;
 	}
-	
+
 	@Transactional
 	public JSONObject getBillDetail(GetBillDetailParameters getBillDetailParameters, String user_info_id) {
 		JSONObject resultJsonObject = new JSONObject();
@@ -94,22 +96,37 @@ public class BillService {
 					ConanExceptionConstants.BILL_NOT_EXISTS_EXCEPTION_MESSAGE,
 					ConanExceptionConstants.BILL_NOT_EXISTS_EXCEPTION_HTTP_STATUS);
 		}
-		if(StringUtils.isNotBlank(userBill.getBill_type())&&userBill.getBill_type().equals("1")) {//充值
-			RechargeBill rechargeBill = rechargeBillMapper.selectByPrimaryKey(userBill.getId());//BILL账单 recharge账单 cost账单使用的ID均为同一个ID
+		resultJsonObject.put("bill_id", userBill.getId());
+		resultJsonObject.put("created_at", userBill.getCreated_at());
+		resultJsonObject.put("bill_type", userBill.getBill_type());
+		resultJsonObject.put("bill_remain", userBill.getRemain_gold());
+		resultJsonObject.put("bill_digest", userBill.getBill_digest());
+		resultJsonObject.put("bill_status", userBill.getBill_status());
+		if (StringUtils.isNotBlank(userBill.getBill_type()) && userBill.getBill_type().equals("1")) {// 充值
+			// BILL账单 recharge账单 cost账单使用的ID均为同一个ID
+			RechargeBill rechargeBill = rechargeBillMapper.selectByPrimaryKey(userBill.getId());
 			if (rechargeBill == null) {
 				throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
 						ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
 						ConanExceptionConstants.INTERNAL_SERVER_ERROR_HTTP_STATUS);
 			}
 			resultJsonObject.put("bill_amount", rechargeBill.getGold_amount());
-		}else if(StringUtils.isNotBlank(userBill.getBill_type())&&userBill.getBill_type().equals("2")) {//支出
-			resultJsonObject.put("bill_id", userBill.getId());
-			resultJsonObject.put("created_at", userBill.getCreated_at());
-			resultJsonObject.put("bill_type", userBill.getBill_type());
-			resultJsonObject.put("bill_remain", userBill.getRemain_gold());
-			resultJsonObject.put("bill_digest", userBill.getBill_digest());
-			resultJsonObject.put("bill_status", userBill.getBill_status());
-			CostRecord costRecord = costRecordMapper.selectByPrimaryKey(userBill.getId());//BILL账单 recharge账单 cost账单使用的ID均为同一个ID
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("recharge_bill_id", rechargeBill.getId());
+			jsonObject.put("created_at", rechargeBill.getCreated_at());
+			jsonObject.put("verified_at", rechargeBill.getVerified_time());
+			jsonObject.put("success_at", rechargeBill.getSuccess_time());
+			jsonObject.put("recharge_type", rechargeBill.getRecharge_type());
+			jsonObject.put("photo", rechargeBill.getPhoto());
+			jsonObject.put("comment", rechargeBill.getComment());
+			jsonObject.put("rmb_amount", rechargeBill.getRmb_amount());
+			jsonObject.put("gold_amount", rechargeBill.getGold_amount());
+			jsonObject.put("gold_coupon", rechargeBill.getGold_coupon());
+			jsonObject.put("gold_total", rechargeBill.getGold_total());
+			resultJsonObject.put("recharge_detail", jsonObject);
+		} else if (StringUtils.isNotBlank(userBill.getBill_type()) && userBill.getBill_type().equals("2")) {// 支出
+			CostRecord costRecord = costRecordMapper.selectByPrimaryKey(userBill.getId());// BILL账单 recharge账单
+																							// cost账单使用的ID均为同一个ID
 			if (costRecord == null) {
 				throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
 						ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
@@ -118,18 +135,30 @@ public class BillService {
 			resultJsonObject.put("bill_amount", costRecord.getCost_gold());
 			resultJsonObject.put("cost_type", costRecord.getCost_type());
 			JSONArray jsonArray = new JSONArray();
-			List<DetectionAccount> detectionAccountList= detectionAccountMapper.selectByRecordIdAndUserInfoId(costRecord.getId(), user_info_id, getBillDetailParameters.getCurrent_page(), ConanApplicationConstants.INIT_PAGE_SIZE);
-			for(DetectionAccount detectionAccount: detectionAccountList) {
-				JSONObject  jsonObject = new JSONObject();
+			List<DetectionAccount> detectionAccountList = detectionAccountMapper.selectByRecordIdAndUserInfoId(
+					costRecord.getId(), user_info_id, getBillDetailParameters.getCurrent_page(),
+					ConanApplicationConstants.INIT_PAGE_SIZE);
+			for (DetectionAccount detectionAccount : detectionAccountList) {
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put("scan_account_id", detectionAccount.getId());
+				jsonObject.put("created_at", detectionAccount.getCreated_at());
+				jsonObject.put("account_name", detectionAccount.getAccount_name());
+				jsonObject.put("account_score", detectionAccount.getAccount_score());
+				jsonObject.put("detail_score0", detectionAccount.getDetail_score0());
+				jsonObject.put("detail_score1", detectionAccount.getDetail_score1());
+				jsonObject.put("detail_score2", detectionAccount.getDetail_score2());
+				jsonObject.put("detail_score3", detectionAccount.getDetail_score3());
+				jsonObject.put("detail_score4", detectionAccount.getDetail_score4());
+				jsonObject.put("scan_cost", detectionAccount.getAccount_score() < 0 ? 0 : 1);
+				jsonArray.add(jsonObject);
 			}
-			
-		}else {//异常
+
+		} else {// 异常
 			throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
 					ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
 					ConanExceptionConstants.INTERNAL_SERVER_ERROR_HTTP_STATUS);
 		}
-		
-		
+
 		return resultJsonObject;
 	}
 }
