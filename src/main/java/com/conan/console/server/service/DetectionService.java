9cd0913b466526e41c99fb28bc1f33c487aa5ab4
sn_baby <sn_baby@qq.com>
2018-05-03 01:24:31 +0800

    修改

--- src/main/java/com/conan/console/server/service/DetectionService.java
+++ src/main/java/com/conan/console/server/service/DetectionService.java
@@ -1,14 +1,12 @@
 package com.conan.console.server.service;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.conan.console.server.entity.master.CostRecord;
 import com.conan.console.server.entity.master.DetectionAccount;
@@ -18,6 +16,7 @@ import com.conan.console.server.mapper.master.DetectionAccountMapper;
 import com.conan.console.server.parameter.UserGetScanHistoryParameters;
 import com.conan.console.server.utils.ConanApplicationConstants;
 import com.conan.console.server.utils.ConanExceptionConstants;
+import com.conan.console.server.utils.ConanUtils;
 
 @Service
 public class DetectionService {
@@ -63,27 +62,24 @@ public class DetectionService {
 
 	@Transactional
 	public JSONObject getTopDangers(int topNum, int lastDays, String user_info_id) {
-		JSONObject jsonObject = new JSONObject();
-		CostRecord costRecord = costRecordMapper.selectLastRecordByUserInfoId(user_info_id);
-		if (costRecord == null) {
-			return jsonObject;
-		}
-		List<DetectionAccount> detectionAccountList = detectionAccountMapper.selectByRecordId(costRecord.getId());
+		JSONObject resultJsonObject = new JSONObject();
+		List<DetectionAccount> detectionAccountList = detectionAccountMapper.selectTopDangersByUserInfoId(user_info_id, topNum, ConanUtils.getLastDay(lastDays));
 		if (detectionAccountList == null || detectionAccountList.isEmpty()) {
-			throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
-					ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
-					ConanExceptionConstants.INTERNAL_SERVER_ERROR_HTTP_STATUS);
+			resultJsonObject.put("topDangers", new JSONArray());
+			return resultJsonObject;
 		}
-		int dangerScanNo = 0;// 危险账号个数
+		JSONArray jsonArray = new JSONArray();  
 		for (DetectionAccount detectionAccount : detectionAccountList) {
-			if (detectionAccount.getAccount_score() >= 80 && detectionAccount.getAccount_score() < 100) {// 80--100危险账号
-				dangerScanNo++;
-			}
+			JSONObject jsonObject = new JSONObject();
+			/*"account_name":"kaka臣",
+	        "account_score":95,
+	        "detection_time":"2018-04-11 11:23:56",*/
+			jsonObject.put("account_name", detectionAccount.getAccount_name());
+			jsonObject.put("account_score", detectionAccount.getAccount_score());
+			jsonObject.put("detection_time", detectionAccount.getCreated_at());
+			jsonArray.add(jsonObject);
 		}
-		jsonObject.put("totalScanNo", detectionAccountList.size());
-		jsonObject.put("dangerScanNo", dangerScanNo);
-		jsonObject.put("dangerPercent", dangerScanNo / detectionAccountList.size());
-		jsonObject.put("recentScanTime", costRecord.getCreated_at());
-		return jsonObject;
+		resultJsonObject.put("topDangers", jsonArray);
+		return resultJsonObject;
 	}
 }
--- src/main/java/com/conan/console/server/utils/ConanUtils.java
+++ src/main/java/com/conan/console/server/utils/ConanUtils.java
@@ -5,11 +5,11 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class ConanUtils {
-	public Date getLastDay(int lastDay) {
+	public static Date getLastDay(int lastDays) {
 		Date date = new Date();// 取时间
 		Calendar calendar = new GregorianCalendar();
 		calendar.setTime(date);
-		calendar.add(calendar.DATE, -1*lastDay);
+		calendar.add(calendar.DATE, -1*lastDays);
 		calendar.set(Calendar.HOUR, 0);
 		calendar.set(Calendar.MINUTE, 0);
 		calendar.set(Calendar.SECOND, 0);
