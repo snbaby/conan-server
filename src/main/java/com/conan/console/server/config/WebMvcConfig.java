50e605b0da571786169668c88bf403bb5fe26670
sn_baby <sn_baby@qq.com>
2018-05-04 16:36:45 +0800

    修改

--- src/main/java/com/conan/console/server/config/WebMvcConfig.java
+++ src/main/java/com/conan/console/server/config/WebMvcConfig.java
@@ -20,6 +20,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 		patterns.add("/api/v2.0/get_validation_code");
 		patterns.add("/api/v2.0/user_login");
 		patterns.add("/api/v2.0/check_validation_code");
+		patterns.add("/api/v2.0/user_reset_passwd");
 		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/**").excludePathPatterns(patterns);
 		super.addInterceptors(registry);
 	}
