6c502463f38398311ea56a8be9e44cd324a8ca1f
wangjian <sn_baby@qq.com>
2018-05-05 11:20:48 +0800

    修改

--- src/main/java/com/conan/console/server/service/MinioService.java
+++ src/main/java/com/conan/console/server/service/MinioService.java
@@ -3,15 +3,32 @@
  */
 package com.conan.console.server.service;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.xmlpull.v1.XmlPullParserException;
+
+import com.conan.console.server.exception.ConanException;
+import com.conan.console.server.utils.ConanExceptionConstants;
 
 import io.minio.MinioClient;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidArgumentException;
+import io.minio.errors.InvalidBucketNameException;
+import io.minio.errors.InvalidEndpointException;
+import io.minio.errors.InvalidExpiresRangeException;
+import io.minio.errors.InvalidPortException;
+import io.minio.errors.NoResponseException;
+import io.minio.errors.RegionConflictException;
 
 /**
  * @author Administrator Paul Wang
@@ -35,19 +52,24 @@ public class MinioService {
 	@Value("${minio.backet-name}")
 	private String minio_bucketName;
 
-	public String uploadFile(InputStream inputStream, String suffix, String contentType) throws Exception {
-		MinioClient minioClient = new MinioClient(minio_endpoint, minio_accessKey, minio_secretKey );
-		if(!minioClient.bucketExists(minio_bucketName)) {
-			minioClient.makeBucket(minio_bucketName);
+	public String uploadFile(InputStream inputStream, String objectName, String contentType) {
+		MinioClient minioClient;
+		String url = "";
+		try {
+			minioClient = new MinioClient(minio_endpoint, minio_accessKey, minio_secretKey );
+			if(!minioClient.bucketExists(minio_bucketName)) {
+				minioClient.makeBucket(minio_bucketName);
+			}
+			minioClient.putObject(minio_bucketName, objectName, inputStream, contentType);
+			url = minioClient.getObjectUrl(minio_bucketName, objectName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new ConanException(ConanExceptionConstants.INTERNAL_SERVER_ERROR_CODE,
+					ConanExceptionConstants.INTERNAL_SERVER_ERROR_MESSAGE,
+					ConanExceptionConstants.INTERNAL_SERVER_ERROR_HTTP_STATUS);
 		}
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
-		String ymd = sdf.format(new Date());
-		String objectName = ymd + "/" + UUID.randomUUID().toString() + (suffix != null ? suffix : "");
-		minioClient.putObject(minio_bucketName, objectName, inputStream, contentType);
-		String url = minioClient.presignedGetObject(minio_bucketName, objectName);
+		
 		return url;
 	}
-	
-	
 	
 }
