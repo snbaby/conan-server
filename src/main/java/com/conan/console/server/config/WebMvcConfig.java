980fbcc779716aefebc4361d4af5326e98583ffc
wangjian <sn_baby@qq.com>
2018-05-06 01:48:23 +0800

    解决跨域问题

--- src/main/java/com/conan/console/server/config/WebMvcConfig.java
+++ src/main/java/com/conan/console/server/config/WebMvcConfig.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.conan.console.server.interceptor.CorsInterceptor;
 import com.conan.console.server.interceptor.TokenInterceptor;
 
 @Configuration
@@ -15,6 +15,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
 		List<String> patterns = new ArrayList<>();
 		patterns.add("/api/v2.0/user_register");
 		patterns.add("/api/v2.0/get_validation_code");
@@ -23,11 +24,5 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 		patterns.add("/api/v2.0/user_reset_passwd");
 		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/**").excludePathPatterns(patterns);
 		super.addInterceptors(registry);
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowCredentials(true)
-				.allowedMethods("GET", "POST", "DELETE", "PUT").maxAge(3600);
 	}
 }
--- src/main/java/com/conan/console/server/interceptor/TokenInterceptor.java
+++ src/main/java/com/conan/console/server/interceptor/TokenInterceptor.java
@@ -13,62 +13,40 @@ import com.conan.console.server.utils.ConanExceptionConstants;
 
 /**
  * 
- * @author Paul token 拦截器，当请求中未带token时，拦截请求，并验证token是否过期
+ * @author Paul session 拦截器，当请求中未带token时，拦截请求，并验证token是否过期
  */
 public class TokenInterceptor implements HandlerInterceptor {
-	private final Logger _logger = LoggerFactory.getLogger(this.getClass()) ;
-	
-	@Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-        _logger.info("this is MyInterceptor1 postHandle");
-    }
+	private final Logger _logger = LoggerFactory.getLogger(this.getClass());
 
-    @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-        _logger.info("this is MyInterceptor1 afterCompletion");
-    }
-	
+	@Override
+	public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o,
+			ModelAndView modelAndView) throws Exception {
+		_logger.info("this is TokenInterceptor postHandle");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			Object o, Exception e) throws Exception {
+		_logger.info("this is TokenInterceptor afterCompletion");
+	}
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		String ip = getIpAddr(request);
-		System.out.println("--------------------IP"+ip);
-		//获取session里的登录状态值
-        String str = (String) request.getSession().getAttribute("isLogin");
-        //如果登录状态不为空则返回true，返回true则会执行相应controller的方法
-        if(str!=null){
-            return true;
-        }else {
-        	throw new ConanException(ConanExceptionConstants.USER_NOT_LOGIN_EXCEPTION_CODE,
-    				ConanExceptionConstants.USER_NOT_LOGIN_EXCEPTION_MESSAGE,
-    				ConanExceptionConstants.USER_NOT_LOGIN_EXCEPTION_HTTP_STATUS);
-        }
+		if(request.getMethod().equals("OPTIONS")) {//解决跨域OPTIONS 问题
+			return true;
+		}else {//session简单检测
+			// 获取session里的登录状态值
+			String str = (String) request.getSession().getAttribute("isLogin");
+			// 如果登录状态不为空则返回true，返回true则会执行相应controller的方法
+			if (str != null) {
+				return true;
+			} else {
+				throw new ConanException(ConanExceptionConstants.USER_NOT_LOGIN_EXCEPTION_CODE,
+						ConanExceptionConstants.USER_NOT_LOGIN_EXCEPTION_MESSAGE,
+						ConanExceptionConstants.USER_NOT_LOGIN_EXCEPTION_HTTP_STATUS);
+			}
+		}
+		
 	}
-	
-	/**
-     * 获取访问的ip地址
-     * 
-     * @param request
-     * @return
-     */
-    public static String getIpAddr(HttpServletRequest request) {
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
-    }
-
 }
