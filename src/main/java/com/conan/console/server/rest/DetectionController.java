6466a0837c5bea6048febfdcc91ee1c5be698a44
sn_baby <sn_baby@qq.com>
2018-05-08 22:09:11 +0800

    修改

--- src/main/java/com/conan/console/server/rest/DetectionController.java
+++ src/main/java/com/conan/console/server/rest/DetectionController.java
@@ -1,15 +1,9 @@
 package com.conan.console.server.rest;
 
-import java.io.IOException;
-import java.util.Date;
-
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.conan.console.server.exception.ConanException;
-import com.conan.console.server.parameter.GetTopDangersParameters;
 import com.conan.console.server.parameter.QueryPreCheckParameters;
 import com.conan.console.server.parameter.UserGetScanHistoryParameters;
 import com.conan.console.server.response.ResponseSuccessResult;
@@ -53,8 +46,8 @@ public class DetectionController {
 		return new ResponseEntity<>(responseResult,HttpStatus.OK);
 	}
 	
-	@PostMapping("user_get_scan_history_export_link")
-	public ResponseEntity<ResponseSuccessResult> userGetScanHistoryExportLink(HttpServletRequest request,@RequestBody @Valid UserGetScanHistoryParameters userGetScanHistoryParameters, BindingResult bindingResult) {
+	@PostMapping("get_scan_history_export_link")
+	public ResponseEntity<ResponseSuccessResult> getScanHistoryExportLink(HttpServletRequest request,@RequestBody @Valid UserGetScanHistoryParameters userGetScanHistoryParameters, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			throw new ConanException(ConanExceptionConstants.PARAMETER_EXCEPTION_CODE,
 					ConanExceptionConstants.PARAMETER_EXCEPTION_MESSAGE, bindingResult.getFieldError(),
