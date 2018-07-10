e04f74d10e0b95facb795336770db25fb57922f1
sn_baby <sn_baby@qq.com>
2018-07-10 12:11:06 +0800

    修改

--- src/main/java/com/conan/console/server/exception/ConanControllerAdvice.java
+++ src/main/java/com/conan/console/server/exception/ConanControllerAdvice.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.conan.console.server.response.ResponseFaildResult;
 import com.conan.console.server.utils.ConanExceptionConstants;
 
